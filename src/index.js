d4bcf1b23ef1c458c099123f4ea9ada79f1b7931
nirch <nirh2@yahoo.com>
2023-04-04 14:34:45 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+const port = 3000;
+
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb://localhost:27017/test';
+const mongo = new MongoClient(uri);
+
+// enable all cors requests
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/posts', (req, res) => {
+  mongo
+    .db()
+    .collection('posts')
+    .find()
+    .toArray()
+    .then(posts => {
+      res.send(posts);
+    });
+});
+
+mongo.connect().then(() => {
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+});
