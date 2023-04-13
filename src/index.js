1c43df1f12f657238bd420eb123211ad453507c7
nirch <nirh2@yahoo.com>
2023-04-13 08:46:46 +0300

    back to initial

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const app = express();
-var cors = require('cors');
 const port = 3000;
 
 const { MongoClient } = require('mongodb');
-const uri = process.env.MONGODB_URI
-  ? process.env.MONGODB_URI
-  : 'mongodb://localhost:27017/test';
+const uri = 'mongodb://localhost:27017/test';
 const mongo = new MongoClient(uri);
-
-// enable all cors requests
-app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
