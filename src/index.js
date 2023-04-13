ed5e3e8cb0383af64d8b6ba75584cfc98c3cc5cf
nirch <nirh2@yahoo.com>
2023-04-13 09:49:38 +0300

    adding cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+var cors = require('cors');
 const app = express();
 const port = 3000;
 
 const { MongoClient } = require('mongodb');
 const uri = 'mongodb://localhost:27017/test';
 const mongo = new MongoClient(uri);
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
